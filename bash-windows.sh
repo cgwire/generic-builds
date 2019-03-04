d3931eab5796cd02a4e79ef68919fb52a925bd91
Antoine Boellinger <antoine.boellinger@unit-image.fr>
2019-03-04 17:23:03 +0100

    WIP (trying to get pyalembic working)

--- bash-windows.sh
+++ bash-windows.sh
@@ -2,9 +2,12 @@
 # You should run it using MINGW64 (ie git-bash from git for windows)
 # Prerequisite:
 # * install cmake and have it in your PATH
-#   PATH=$PATH:/d/dev/cmake-3.13.4-win64-x64/bin/
+#   PATH=${PATH}:/d/dev/cmake-3.13.4-win64-x64/bin/
 # * have devenv in your PATH
-#   PATH=$PATH:/c/Program\ Files\ \(x86\)/Microsoft\ Visual\ Studio\ 14.0/Common7/IDE/
+#   PATH=${PATH}:/c/Program\ Files\ \(x86\)/Microsoft\ Visual\ Studio\ 14.0/Common7/IDE/
+# * have mt in your PATH (boost)
+#   PATH=PATH=${PATH}:/c/Program\ Files\ \(x86\)/Microsoft\ SDKs/Windows/v7.1A/Bin
+
 
 ROOT=$( pwd )
 VS_VERSION="Visual Studio 14 2015 Win64"
@@ -26,142 +29,107 @@ VS_VERSION="Visual Studio 14 2015 Win64"
 #- script: |
 #    wget https://github.com/openexr/openexr/releases/download/v2.3.0/ilmbase-2.3.0.tar.gz
 #  displayName: Download ILMLab archive
+
+    # ZLIB ---------------------------------------------------------------------
     # No wget on MINGW64, using curl instead (-L flag to follow redirect)
-    cd $ROOT
-    curl -L https://github.com/openexr/openexr/releases/download/v2.3.0/ilmbase-2.3.0.tar.gz --output ilmbase-2.3.0.tar.gz
+    curl -L https://zlib.net/zlib1211.zip --output zlib.zip
+    unzip zlib.zip
+    cmake -G "${VS_VERSION}"                                                  \
+          -S "${ROOT}/zlib"                                                   \
+          -B "${ROOT}/zlib/build"                                             \
+          -DCMAKE_INSTALL_PREFIX="${ROOT}/dep/zlib"
 
-#- task: ExtractFiles@1
-#  inputs:
-#    archiveFilePatterns: '*.tar.gz'
-#    destinationFolder: ilmbase-2.3.0
-#    cleanDestinationFolder: true
-#  displayName: Extract ILMLab archive
+    cmake --build zlib/build --target INSTALL --config release
+
+    # I found zlib1.dll to be already installed on my system,
+    # so getting the headers is enough, I don't build the library
+    # --------------------------------------------------------------------------
+
+    # BOOST --------------------------------------------------------------------
+    curl -L https://sourceforge.net/projects/boost/files/boost/1.60.0/boost_1_60_0.zip/download --output boost.zip
+    unzip boost.zip
+    cd boost
+    ./bootstrap
+    #./b2  # Builds boost -> coffee time !
+    #./b2 --prefix=${ROOT}/toto --with-python toolset=msvc address-model=64 link=shared variant=release install
+    #b2 --prefix="${ROOT}/dep/Boost" --with-python -j8 --build-type=complete link=static,shared address-model=64 install
+    ./b2 --prefix="${ROOT}/dep/Boost" --with-python --with-program_options -j8 toolset=msvc-14.0 link=static,shared address-model=64 install
+    # Note that I experienced some difficulties building the boost dll
+    # We have to rename boost libs for cmake to find them (b2 might have an argument for that)
+    cd ${ROOT}
+    # --------------------------------------------------------------------------
+
+    # OPENEXR ------------------------------------------------------------------
+    # We get the whole source code, because most releases has their CMAKE files stripped of
+    curl -L https://github.com/openexr/openexr/archive/v2.3.0.tar.gz --output openexr.tar.gz
     # Extract
-    tar -xvf ilmbase-2.3.0.tar.gz
+    tar -xvf openexr.tar.gz
 
-#- script: |
-#    sudo mkdir /usr/local/lib/ilmbase
-#    ls
-#    cd ilmbase-2.3.0
-#    ./bootstrap
-#    ./configure --prefix=/usr/local/lib/ilmbase
-#    sudo make -j4
-#    sudo make install
-#    cd /usr/local/lib/ilmbase
-#    sudo tar -cf ilmbase-2.3.0.tar *
-#    sudo gzip ilmbase-2.3.0.tar
-#  displayName: Build ILMBase
-#  workingDirectory: ilmbase-2.3.0
-    # Following install instructions from ilmbase/README.md
-    cd "$ROOT/ilmbase-2.3.0"
-    mkdir "$ROOT/lib/ilmbase"
-    # Create solution
-    cmake -DCMAKE_INSTALL_PREFIX="$ROOT/lib/ilmbase" \
-          -G "$VS_VERSION" -B build
-    # Build
-    cmake --build ./build --target INSTALL --config release
+    # Deploy bug https://github.com/openexr/openexr/issues/355
+    sed -i "s/OPENEXR_PACKAGE_PREFIX/CMAKE_INSTALL_PREFIX/g" openexr/OpenEXR/IlmImf/CMakeLists.txt
 
+    cmake -G "${VS_VERSION}"                                                  \
+          -S "${ROOT}/openexr"                                                \
+          -B "${ROOT}/openexr/build"                                          \
+          -DCMAKE_INSTALL_PREFIX="${ROOT}/dep/openexr"                        \
+          -DZLIB_INCLUDE_DIR="${ROOT}/dep/zlib/include"                       \
+          -DZLIB_LIBRARY="${ROOT}/dep/zlib/lib/zlib.lib"                      \
+          -DBoost_INCLUDE_DIR="${ROOT}/dep/Boost/include/boost-1_60/"         \
+          -DBoost_LIBRARIES="${ROOT}/dep/Boost/lib"                           \
+          -DPYTHON_INCLUDE_DIR="${ADSK_3DSMAX_x64_2018}/python/include"       \
+          -DPYTHON_LIBRARY="${ADSK_3DSMAX_x64_2018}/python/libs/python27.lib" \
+          -DPYTHON_EXECUTABLE="${ADSK_3DSMAX_x64_2018}/3dsmaxpy.exe"          \
+          -DNUMPY_INCLUDE_DIRS="${ADSK_3DSMAX_x64_2018}/python/Lib/site-packages/numpy/core/include"
 
-#- script: |
-#    wget https://github.com/openexr/openexr/releases/download/v2.3.0/pyilmbase-2.3.0.tar.gz
-#  displayName: Download PyILMLab archive
-    cd $ROOT
-    curl -L https://github.com/openexr/openexr/releases/download/v2.3.0/pyilmbase-2.3.0.tar.gz --output pyilmbase-2.3.0.tar.gz
+        # Attention à bien utiliser les headers générés par b2 et pas ceux des sources de boost !!!
 
-#- task: ExtractFiles@1
-#  inputs:
-#    archiveFilePatterns: '*.tar.gz'
-#    destinationFolder: pyilmbase-2.3.0
-#    cleanDestinationFolder: true
-#  displayName: Extract PyILMLab archive
-    tar -xvf pyilmbase-2.3.0.tar.gz
+#          -DOPENEXR_BUILD_OPENEXR=OFF                              \
+    # Building for max requires to point to the max distributed python.lib
 
-#- script: |
-#    sudo mkdir /usr/local/lib/pyilmbase
-#    sudo pip install numpy
-#    export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/lib/ilmbase/lib
-#    cd pyilmbase-2.3.0
-#    ./configure --prefix=/usr/local/lib/pyilmbase --with-ilmbase-prefix=/usr/local/lib/ilmbase
-#    sudo make -j4
-#    sudo make install
-#    cd /usr/local/lib/pyilmbase
-#    sudo tar -czf pyilmbase-2.3.0.tar.gz *
-#  displayName: Build PyILMBase
-#  workingDirectory: pyilmbase-2.3.0
-    # Following install instructions from ilmbase/README.md
-    cd "$ROOT/pyilmbase-2.3.0"
-    mkdir "$ROOT/lib/pyilmbase"
-    # Create solution
-    cmake -DILMBASE_PACKAGE_PREFIX="$ROOT/lib/ilmbase" \
-          -DCMAKE_INSTALL_PREFIX="$ROOT/lib/pyilmbase" \
-          -G "$VS_VERSION" -B build
-    # Build
-    cmake --build ./build --target INSTALL --config release
+    cmake --build openexr/build --target INSTALL --config release
 
-- script: |
-    wget https://github.com/openexr/openexr/releases/download/v2.3.0/openexr-2.3.0.tar.gz
-  displayName: Download OpenEXR archive
+    # --------------------------------------------------------------------------
 
-- task: ExtractFiles@1
-  inputs:
-    archiveFilePatterns: '*.tar.gz'
-    destinationFolder: openexr-2.3.0
-    cleanDestinationFolder: true
-  displayName: Extract OpenEXR archive
+    # HDF5 ---------------------------------------------------------------------
+    #curl -L https://support.hdfgroup.org/ftp/HDF5/releases/hdf5-1.10/hdf5-1.10.5/src/hdf5-1.10.5.tar.gz --output hdf5.tar.gz
+    curl -L https://support.hdfgroup.org/ftp/HDF5/releases/hdf5-1.10/hdf5-1.10.5/src/hdf5-1.10.5.tar.gz --output hdf5.tar.gz
+    tar -xvf hdf5.tar.gz
 
-- script: |
-    ls
-    cd openexr-2.3.0
-    sudo mkdir /usr/local/lib/openexr
-    ./bootstrap
-    ./configure --prefix=/usr/local/lib/openexr --with-ilmbase-prefix=/usr/local/lib/ilmbase --enable-imfexamples
-    sudo make -j4
-    sudo make install
-    cd /usr/local/lib/openexr
-    sudo tar -czf openexr-2.3.0.tar.gz *
-  displayName: Build OpenEXR
-  workingDirectory: openexr-2.3.0
+    cmake -G "${VS_VERSION}"                                                  \
+          -S "${ROOT}/hdf5"                                                   \
+          -B "${ROOT}/hdf5/build"                                             \
+          -DCMAKE_INSTALL_PREFIX="${ROOT}/dep/hdf5"
 
-- script: |
-    wget https://github.com/alembic/alembic/archive/1.7.10.tar.gz
-  displayName: Download Alembic archive
+    cmake --build hdf5/build --target INSTALL --config release
+    # --------------------------------------------------------------------------
 
-- task: ExtractFiles@1
-  inputs:
-    archiveFilePatterns: '*.tar.gz'
-    destinationFolder: alembic-1.7.10
-    cleanDestinationFolder: true
-  displayName: Extract Alembic archive
+    # ALEMBIC ------------------------------------------------------------------
+    #curl -L https://github.com/alembic/alembic/archive/1.7.10.tar.gz --output alembic.tar.gz
+    curl -L https://github.com/alembic/alembic/archive/1.7.10.tar.gz --output alembic.tar.gz
+    tar -xvf alembic.tar.gz
 
-- script: |
-    sudo mkdir alembic
-    ls
-    cd alembic-1.7.10
-    ./bootstrap
-    cmake -DUSE_HDF5=ON -DUSE_EXEMPLES=ON -DALEMBIC_LIB_USES_TR1=ON -DALEMBIC_LIB_USES_BOOST=OFF -DILM_INCLUDE_DIR=/usr/local/lib/ilmbase/include/OpenEXR ../alembic-1.7.10 -DCMAKE_INSTALL_PREFIX=/usr/local/lib/alembic
-    sudo make -j4
-    sudo make install
-    cd /usr/local/lib/alembic
-    sudo tar -czf alembic-1.7.10.tar.gz *
-  displayName: Build Alembic
-  workingDirectory: alembic-1.7.10
+    cmake -G "${VS_VERSION}"                                                  \
+          -S "${ROOT}/alembic"                                                \
+          -B "${ROOT}/alembic/build"                                          \
+          -DUSE_PYALEMBIC=ON                                                  \
+          -DUSE_HDF5=ON                                                       \
+          -DUSE_STATIC_HDF5=ON                                                \
+          -DUSE_STATIC_BOOST=ON                                               \
+          -DUSE_TESTS=OFF                                                      \
+          -DILMBASE_ROOT="${ROOT}/dep/openexr"                                \
+          -DHDF5_ROOT="${ROOT}/dep/hdf5"                                      \
+          -DBOOST_INCLUDE_DIR="${ROOT}/dep/Boost/include/boost_1_60_0"          \
+          -DBOOST_LIBRARY_DIR="${ROOT}/dep/Boost/lib"                          \
+          -DALEMBIC_PYILMBASE_INCLUDE_DIRECTORY="${ROOT}/dep/openexr/include"          \
+          -DALEMBIC_PYIMATH_MODULE_DIRECTORY="${ROOT}/dep/openexr/lib/python2.7/site-packages/" \
+          -DPYTHON_EXECUTABLE="${ADSK_3DSMAX_x64_2018}/3dsmaxpy.exe"                            \
+          -DPYTHON_INCLUDE_DIR="${ADSK_3DSMAX_x64_2018}/python/include"                         \
+          -DPYTHON_LIBRARY="${ADSK_3DSMAX_x64_2018}/python/libs/python27.lib"                   \
+          -DCMAKE_INSTALL_PREFIX="${ROOT}/dep/alembic"
 
-- script: |
-    ls
-    cp /usr/local/lib/openexr/openexr-2.3.0.tar.gz $(Build.ArtifactStagingDirectory)
-    cp /usr/local/lib/ilmbase/ilmbase-2.3.0.tar.gz $(Build.ArtifactStagingDirectory)
-    cp /usr/local/lib/pyilmbase/pyilmbase-2.3.0.tar.gz $(Build.ArtifactStagingDirectory)
-    cp /usr/local/lib/alembic/alembic-1.7.10.tar.gz $(Build.ArtifactStagingDirectory)
-    ls $(Build.ArtifactStagingDirectory)
-  displayName: Prepare Release
+          #-DBOOST_INCLUDEDIR="${ROOT}/dep/Boost/include/boost-1_60/"          \
+          #-DBOOST_LIBRARYDIR="${ROOT}/dep/Boost/lib"                          \
+          #-DUSE_STATIC_BOOST=1                                                        \
+    cmake --build alembic/build --target INSTALL --config release
 
-- task: GitHubRelease@0
-  inputs:
-    gitHubConnection: GitHub connection 1
-    repositoryName: cgwire/generic-builds
-    action: create
-    target: 3de0dbd1407c8bc44b373fe425c323d8da02f3f1
-    tagSource: auto
-    tag: 2.3.0
-    assetUploadMode: 'replace'
-  displayName: Release to Github
+    # --------------------------------------------------------------------------
